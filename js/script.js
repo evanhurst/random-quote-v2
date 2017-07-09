741f0445356d47dd0ed6fc3e30195713effa9979
Evan Hurst <hurst.evan@gmail.com>
2017-07-09 14:15:37 -0400

    -Added category tag

--- js/script.js
+++ js/script.js
@@ -1,19 +1,25 @@
 //Object Definition
-function Quote(quote, source, citation, year, id) {
+function Quote(quote, source, citation, year, id, category) {
     this.quote = quote;
     this.source = source;
-    this.citation = citation || "";
-    this.year = year || "";
+    this.citation = citation;
+    this.year = year;
     this.id = id;
+    this.category = category;
 }
 //Global Variables
 let quotes = [
-    new Quote("Don\'t cry because it\'s over, smile because it happened.", "Dr. Suess", 0),
-    new Quote("Do not take life too seriously. You will never get out of it alive.", "Elbert Hubbard", "Life", "1942", 1),
-    new Quote("I am not a member of any organized political party. I am a democrat.", "Will Rogers", 2),
-    new Quote("Always remember that you are absolutely unique. Just like everyone else.", "Margaret Mead", "Unknown", 3),
-    new Quote("Laugh and the world laughs with you, snore an you sleep alone.", "Anthony Burgess", "Every Night", "1992", 4),
-    new Quote("I can resist everything except temptation.", "Oscar Wilde", 5)
+    new Quote("Don\'t cry because it\'s over, smile because it happened.", "Dr. Suess", null, null, 0, "Inspiration"),
+    
+    new Quote("Do not take life too seriously. You will never get out of it alive.", "Elbert Hubbard", "Life", "1942", 1, "Funny"),
+    
+    new Quote("I am not a member of any organized political party. I am a democrat.", "Will Rogers", null, null, 2, "Politics"),
+    
+    new Quote("Always remember that you are absolutely unique. Just like everyone else.", "Margaret Mead", "Unknown", null, 3, "Funny"),
+    
+    new Quote("Laugh and the world laughs with you, snore an you sleep alone.", "Anthony Burgess", "Every Night", "1992", 4, "Funny"),
+    
+    new Quote("I can resist everything except temptation.", "Oscar Wilde", null, null, 5, "Funny")
 ];
 let usedQuotes = [];
 
@@ -44,13 +50,28 @@ function getRandomQuote(){
 
 function printQuote() {
     let randomQuote = getRandomQuote();
-    let quoteString =     '<p class="quote">' + randomQuote.quote + '</p><p class="source">' + randomQuote.source + '<span class="citation">'+ randomQuote.citation +'</span><span class="year">'+ randomQuote.year + '</span></p>';
+    //Build the quote HTML
+    let quoteString = '<p class="quote">' + randomQuote.quote + '</p><p class="source">' + randomQuote.source; 
+        
+    //Optional Citation
+    if(randomQuote.citation) {
+        quoteString += '<span class="citation">'+ randomQuote.citation + '</span>';
+    };
     
-    console.log(quoteString);
+    //Optional Year
+    if(randomQuote.year) {
+        quoteString += '<span class="year">'+ randomQuote.year + '</span>';
+    };
+    
+    quoteString += '</p>';
+    quoteString += '<p class="category">' + randomQuote.category + '</p>';
     
     document.getElementById('quote-box').innerHTML = quoteString;
     
+    //Set background to a random color
     document.getElementsByTagName('body')[0].style.backgroundColor = getRandomColor();
+    
+    console.log(randomQuote.category);
 }
     
 function getRandomColor() {
